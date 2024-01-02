9a080c05329de5548813b8842bfb02d38092b2fa
abdusamad <abdusamad2609@gmail.com>
2024-01-02 19:36:27 +0500

    ozgarishlar -m

---
+++ HistoryCell.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryCell.swift
+//  Elektron Tasbeh
+//
+//  Created by Abdusamad Mamasoliyev on 25/12/23.
+//
+
+import UIKit
+
+class HistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HistoryDM.swift
@@ -0,0 +1,8 @@
+//
+//  HistoryDM.swift
+//  Elektron Tasbeh
+//
+//  Created by Abdusamad Mamasoliyev on 25/12/23.
+//
+
+import Foundation
